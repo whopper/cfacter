b033ec51d7a1960258a50e23a5b70de3b62d8832
Will Hopper <whopper@puppetlabs.com>
2014-08-21 14:43:50 -0700

    Add structured operating system fact

--- lib/src/facts/posix/operating_system_resolver.cc
+++ lib/src/facts/posix/operating_system_resolver.cc
@@ -2,6 +2,7 @@
 #include <facter/facts/posix/os.hpp>
 #include <facter/facts/posix/os_family.hpp>
 #include <facter/facts/scalar_value.hpp>
+#include <facter/facts/map_value.hpp>
 #include <facter/facts/collection.hpp>
 #include <facter/facts/fact.hpp>
 #include <map>
@@ -14,6 +15,7 @@ namespace facter { namespace facts { namespace posix {
         resolver(
             "operating system",
             {
+                fact::os,
                 fact::operating_system,
                 fact::os_family,
                 fact::operating_system_release,
@@ -25,29 +27,99 @@ namespace facter { namespace facts { namespace posix {
     void operating_system_resolver::resolve_facts(collection& facts)
     {
         // Resolve all operating system related facts
+        resolve_structured_operating_system(facts);
         resolve_operating_system(facts);
         resolve_os_family(facts);
         resolve_operating_system_release(facts);
         resolve_operating_system_major_release(facts);
     }
 
-    void operating_system_resolver::resolve_operating_system(collection& facts)
+    void operating_system_resolver::resolve_structured_operating_system(collection& facts)
     {
-        // Default to the same value as the kernel
-        auto kernel = facts.get<string_value>(fact::kernel);
-        if (!kernel) {
-            return;
+        auto os_value = make_value<map_value>();
+        auto operating_system = determine_operating_system(facts);
+        auto os_family = determine_os_family(facts, operating_system);
+        auto release = determine_operating_system_release(facts);
+
+        if (!operating_system.empty()) {
+            os_value->add("name", make_value<string_value>(operating_system));
         }
 
-        facts.add(fact::operating_system, make_value<string_value>(kernel->value()));
+        if (!os_family.empty()) {
+            os_value->add("family", make_value<string_value>(os_family));
+        }
+
+        if (!release.empty()) {
+            auto release_value = make_value<map_value>();
+            release_value->add("full", make_value<string_value>(release));
+            os_value->add("release", move(release_value));
+        }
+
+        if (!os_value->empty()) {
+            facts.add(fact::os, move(os_value));
+        }
+    }
+
+    void operating_system_resolver::resolve_operating_system(collection& facts)
+    {
+        auto os_value = facts.get<map_value>(fact::os, false);
+        if (!os_value) {
+          return;
+        }
+
+        auto operating_system = os_value->get<string_value>("name");
+        if (operating_system) {
+            facts.add(fact::operating_system, make_value<string_value>(operating_system->value()));
+        }
     }
 
     void operating_system_resolver::resolve_os_family(collection& facts)
     {
-        // Get the operating system fact
-        auto os = facts.get<string_value>(fact::operating_system, false);
+        auto os_value = facts.get<map_value>(fact::os, false);
+        if (!os_value) {
+          return;
+        }
+
+        auto os_family = os_value->get<string_value>("family");
+        if (os_family) {
+            facts.add(fact::os_family, make_value<string_value>(os_family->value()));
+        }
+    }
+
+    void operating_system_resolver::resolve_operating_system_release(collection& facts)
+    {
+        auto os_value = facts.get<map_value>(fact::os, false);
+        if (!os_value) {
+          return;
+        }
+
+        auto release_value = os_value->get<map_value>("release");
+        if (!release_value) {
+          return;
+        }
+
+        auto operating_system_release = release_value->get<string_value>("full");
+        if (operating_system_release) {
+            facts.add(fact::operating_system_release, make_value<string_value>(operating_system_release->value()));
+        }
+    }
+
+    string operating_system_resolver::determine_operating_system(collection& facts)
+    {
+        // Default to the same value as the kernel
+        auto kernel = facts.get<string_value>(fact::kernel);
+        if (!kernel) {
+            return "";
+        } else {
+            return kernel->value();
+        }
+    }
+
+    string operating_system_resolver::determine_os_family(collection& facts, string& operating_system)
+    {
+        auto os = operating_system;
         string value;
-        if (os) {
+        if (!os.empty()) {
             static map<string, string> const systems = {
                 { string(os::redhat),                   string(os_family::redhat) },
                 { string(os::fedora),                   string(os_family::redhat) },
@@ -81,7 +153,7 @@ namespace facter { namespace facts { namespace posix {
                 { string(os::mandriva),                 string(os_family::mandrake) },
                 { string(os::mageia),                   string(os_family::mandrake) },
             };
-            auto const& it = systems.find(os->value());
+            auto const& it = systems.find(os);
             if (it != systems.end()) {
                 value = it->second;
             }
@@ -91,22 +163,22 @@ namespace facter { namespace facts { namespace posix {
             // Default to the same value as the kernel
             auto kernel = facts.get<string_value>(fact::kernel);
             if (!kernel) {
-                return;
+                return "";
             }
             value = kernel->value();
         }
-        facts.add(fact::os_family, make_value<string_value>(move(value)));
+      return value;
     }
 
-    void operating_system_resolver::resolve_operating_system_release(collection& facts)
+    string operating_system_resolver::determine_operating_system_release(collection& facts)
     {
         // Default to the same value as the kernelrelease fact
         auto release = facts.get<string_value>(fact::kernel_release);
         if (!release) {
-            return;
+            return "";
+        } else {
+            return release->value();
         }
-
-        facts.add(fact::operating_system_release, make_value<string_value>(release->value()));
     }
 
 }}}  // namespace facter::facts::posix
